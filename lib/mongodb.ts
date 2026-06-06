28f944b852253d96f450cb45588e88881a1f55cc
hesam <hesvmpiri@gmail.com>
2026-06-06 23:40:02 +0330

    add mongodb connection

---
+++ lib/mongodb.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.DATABASE_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in your environment variables");
+}
+
+export async function connectDB() {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(MONGODB_URI!);
+    console.log("Database connected ✅");
+  } catch {
+    console.log("There is somthing wrong with the database connection");
+  }
+}
