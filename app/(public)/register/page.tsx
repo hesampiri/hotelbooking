da15015b8c27b761610ec15f35e4fd2dfb90a426
hesam <hesvmpiri@gmail.com>
2026-06-17 10:19:57 +0330

    add registeration routes

---
+++ app/(public)/register/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from "@/components/auth/singup-form";
+
+const signup = () => {
+  return (
+    <main className="min-h-screen pt-10 sm:pt-16 md:pt-24 px-4">
+      <SignUpForm />
+    </main>
+  );
+};
+
+export default signup;
---
+++ app/(public)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import SignInForm from "@/components/auth/signin-form";
+import React from "react";
+
+const Signin = () => {
+  return (
+    <main className="min-h-screen pt-10 sm:pt-16 md:pt-24 px-4">
+      <SignInForm />
+    </main>
+  );
+};
+
+export default Signin;
