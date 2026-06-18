7fd3e94d438321fb7f67904bf9cf7ee795f222cb
hesam <hesvmpiri@gmail.com>
2026-06-18 12:44:55 +0330

    add hotel details page

---
+++ app/(public)/hotel/[id]/page.tsx
@@ -0,0 +1,12 @@
+import HotelDetails from "@/components/hotel/hotelDetails"
+
+const HotelDetailsPage = () => {
+
+  return (
+    <div className="xl:px-60 md:px-10 sm:px-5">
+        <HotelDetails/>
+    </div>
+  )
+}
+
+export default HotelDetailsPage
