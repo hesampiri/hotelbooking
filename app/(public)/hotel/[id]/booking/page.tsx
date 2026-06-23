140fc7c6d723edfdfc409fb740668d45ce5e1d93
hesam <hesvmpiri@gmail.com>
2026-06-23 18:15:24 +0330

    add booking route

---
+++ app/(public)/hotel/[id]/booking/page.tsx
@@ -0,0 +1,11 @@
+import BookingForm from "@/components/BookingForm";
+
+const bookingPage = () => {
+  return (
+    <div className="xl:px-60 md:px-10 sm:px-5">
+      <BookingForm />
+    </div>
+  );
+};
+
+export default bookingPage;
