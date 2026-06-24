1c9d6764c2899b32c7b749b43517bf684c687e41
hesam <hesvmpiri@gmail.com>
2026-06-24 13:54:50 +0330

    add  seperated route for searchfilters

---
+++ app/(public)/hotel/search/page.tsx
@@ -0,0 +1,11 @@
+import HotelSearchList from "@/components/hotel/hotelSearchList";
+
+const hotelSearchPage = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-6">
+      <HotelSearchList />
+    </div>
+  );
+};
+
+export default hotelSearchPage;
