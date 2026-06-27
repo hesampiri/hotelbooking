1b628f53c73034976767a160af6a1455e01db0fb
hesam <hesvmpiri@gmail.com>
2026-06-27 23:17:14 +0330

    add review functions

--- features/hotel/queries/hotelApi.ts
+++ features/hotel/queries/hotelApi.ts
@@ -1,4 +1,4 @@
-import { da } from "date-fns/locale";
+import { ReviewType } from "@/types";
 
 export async function getLocations() {
   const res = await fetch("/api/hotels/locations");
@@ -7,7 +7,6 @@ export async function getLocations() {
   return data.locations;
 }
 export async function getHotels(params?: Record<string, string>) {
-  // Construct query string if params are provided
   let url = "/api/hotels";
   if (params && Object.keys(params).length > 0) {
     const query = new URLSearchParams(params).toString();
@@ -19,13 +18,17 @@ export async function getHotels(params?: Record<string, string>) {
   return data.hotels;
 }
 
-export async function getHotelDetails(id:string){
-  const res = await fetch(`/api/hotels/${id}`)
-  const data = await res.json()
-  return data.hotel
+export async function getHotelDetails(id: string) {
+  const res = await fetch(`/api/hotels/${id}`);
+  const data = await res.json();
+  return data.hotel;
 }
 
-export async function getRooms(hotelId:string ,params?: Record<string, string> ){
+export async function getRooms(
+  hotelId: string,
+  params?: Record<string, string>,
+) {
+  
   let url = `/api/hotels/${hotelId}/rooms`;
   if (params && Object.keys(params).length > 0) {
     const query = new URLSearchParams(params).toString();
@@ -35,4 +38,40 @@ export async function getRooms(hotelId:string ,params?: Record<string, string> )
   const data = await res.json();
 
   return data.rooms;
+}
+
+// ✅ Fixed
+export async function getReviews({
+  pageParam = 1,
+  hotelId,
+}: {
+  pageParam?: number;
+  hotelId: string;
+}): Promise<{
+  reviews: ReviewType[];
+  total: number;
+  hasMore: boolean;
+  page: number;
+}> {
+  const res = await fetch(
+    `/api/hotels/${hotelId}/reviews?page=${pageParam}&limit=3`,
+  );
+  if (!res.ok) throw new Error("Failed to fetch reviews");
+
+  const data = await res.json();
+  return data;
+}
+
+export async function postReview(
+  hotelId: string,
+  body: { rating: number; comment: string },
+): Promise<ReviewType> {
+  const res = await fetch(`/api/hotels/${hotelId}/reviews`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+  if (!data.success) throw new Error(data.message);
+  return data.review;
 }
