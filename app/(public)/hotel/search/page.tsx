47345a559aded28bce41f38996d7f26119b642b6
hesam <hesvmpiri@gmail.com>
2026-06-27 23:14:34 +0330

    add suspence to avoid buildtime error

--- app/(public)/hotel/search/page.tsx
+++ app/(public)/hotel/search/page.tsx
@@ -1,9 +1,13 @@
 import HotelSearchList from "@/components/hotel/hotelSearchList";
+import HotelSearchCardSkeleton from "@/components/hotelSearchCardSkeleton";
+import { Suspense } from "react";
 
 const hotelSearchPage = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 py-6">
-      <HotelSearchList />
+      <Suspense fallback={<HotelSearchCardSkeleton />}>
+        <HotelSearchList />
+      </Suspense>
     </div>
   );
 };
