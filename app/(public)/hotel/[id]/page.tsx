d59c6e1dd143b0f2d4b0b2c5f2144450d1843468
hesam <hesvmpiri@gmail.com>
2026-06-27 23:10:05 +0330

    add footer to pages

--- app/(public)/hotel/[id]/page.tsx
+++ app/(public)/hotel/[id]/page.tsx
@@ -1,12 +1,15 @@
-import HotelDetails from "@/components/hotel/hotelDetails"
+import Footer from "@/components/Footer";
+import HotelDetails from "@/components/hotel/hotelDetails";
 
 const HotelDetailsPage = () => {
-
   return (
-    <div className="xl:px-60 md:px-10 sm:px-5">
-        <HotelDetails/>
+    <div>
+      <div className="xl:px-60 md:px-10 sm:px-5">
+        <HotelDetails />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HotelDetailsPage
+export default HotelDetailsPage;
--- app/(public)/hotel/page.tsx
+++ app/(public)/hotel/page.tsx
@@ -1,21 +1,21 @@
+import Footer from "@/components/Footer";
 import HotelList from "@/components/hotel/hotelList";
 import { DatePickerRange } from "@/components/search/datePickerRange";
 import SearchButton from "@/components/search/searchButton";
 import SearchInput from "@/components/search/searchInput";
 
-
 const HotelPage = () => {
-  
   return (
-    <div className="xl:px-60 md:px-10 sm:px-5 ">
-      <div className="flex flex-col sm:flex-row w-full mx-auto items-stretch sm:items-start gap-2 py-6 px-2 ">
-        <SearchInput />
-        <DatePickerRange />
-        <SearchButton/>
+    <div>
+      <div className="xl:px-60 md:px-10 sm:px-5 ">
+        <div className="flex flex-col sm:flex-row w-full mx-auto items-stretch sm:items-start gap-2 py-6 px-2 ">
+          <SearchInput />
+          <DatePickerRange />
+          <SearchButton />
+        </div>
+        <HotelList />
       </div>
-      <div>
-        <HotelList/>
-      </div>
+      <Footer/>
     </div>
   );
 };
