d5704e32bc75ca7365fe6fd6be7999ebc6afd8f9
hesam <hesvmpiri@gmail.com>
2026-07-02 21:35:25 +0330

    chnage hotelList rendering to SSG

--- app/(public)/hotel/page.tsx
+++ app/(public)/hotel/page.tsx
@@ -3,21 +3,36 @@ import HotelList from "@/components/hotel/hotelList";
 import { DatePickerRange } from "@/components/search/datePickerRange";
 import SearchButton from "@/components/search/searchButton";
 import SearchInput from "@/components/search/searchInput";
+import { getHotels } from "@/features/hotel/queries/hotelApi";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
 
-const HotelPage = () => {
+export default async function HotelPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["hotels"],
+    queryFn: ()=> getHotels(),
+  });
+
   return (
     <div>
-      <div className="xl:px-60 md:px-10 sm:px-5 ">
-        <div className="flex flex-col sm:flex-row w-full mx-auto items-stretch sm:items-start gap-2 py-6 px-2 ">
+      <div className="xl:px-60 md:px-10 sm:px-5">
+        <div className="flex flex-col sm:flex-row w-full mx-auto items-stretch sm:items-start gap-2 py-6 px-2">
           <SearchInput />
           <DatePickerRange />
           <SearchButton />
         </div>
-        <HotelList />
+
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <HotelList />
+        </HydrationBoundary>
       </div>
-      <Footer/>
+
+      <Footer />
     </div>
   );
-};
-
-export default HotelPage;
+}
