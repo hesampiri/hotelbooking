22d840effcc1b7f3816e8183433cb7375a71a55e
hesam <hesvmpiri@gmail.com>
2026-07-02 21:35:59 +0330

    change the hotelDetails redering to SSR

--- app/(public)/hotel/[id]/page.tsx
+++ app/(public)/hotel/[id]/page.tsx
@@ -1,11 +1,29 @@
 import Footer from "@/components/Footer";
 import HotelDetails from "@/components/hotel/hotelDetails";
+import { getHotelDetails } from "@/features/hotel/queries/hotelApi";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
 
-const HotelDetailsPage = () => {
+const HotelDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["hotelDetails", id],
+    queryFn: () => getHotelDetails(id),
+  });
   return (
     <div>
       <div className="xl:px-60 md:px-10 sm:px-5">
-        <HotelDetails />
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <HotelDetails />
+        </HydrationBoundary>
       </div>
       <Footer />
     </div>
